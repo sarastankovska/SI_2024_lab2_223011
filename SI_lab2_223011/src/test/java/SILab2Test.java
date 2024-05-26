9790d0863a3b839acb55aae18c6cca8675ffa0a3
sarastankovska <117545822+sarastankovska@users.noreply.github.com>
2024-05-26 22:26:08 +0200

    Update SILab2Test.java

--- SI_lab2_223011/src/test/java/SILab2Test.java
+++ SI_lab2_223011/src/test/java/SILab2Test.java
@@ -9,7 +9,7 @@ class SILab2Test {
     private List<Item> items(Item... items) {
         return new ArrayList<>(Arrays.asList(items));
     }
-    @Test
+ @Test
     void EveryBranch() {
         RuntimeException exception;
         exception = assertThrows(RuntimeException.class, () -> SILab2.checkCart(null, 1));
@@ -18,17 +18,17 @@ class SILab2Test {
         assertFalse(SILab2.checkCart(new ArrayList<Item>(), -1));
         exception = assertThrows(RuntimeException.class, () -> SILab2.checkCart(items(new Item("", null, 20, 0.5f)), 1));
         assertTrue(exception.getMessage().contains("Nema barkod!"));
-        assertFalse(SILab2.checkCart(items(new Item("", "02345", 500, 0.4f)), 2));
-        exception = assertThrows(RuntimeException.class, () -> SILab2.checkCart(items(new Item("Item1", "01234", 500, 0.2f)), 1));
-        assertTrue(exception.getMessage().contains("Greska vo barkodot!"));
+        assertFalse(SILab2.checkCart(items(new Item("", "012345", 500, 0.4f)), 2));
+        exception = assertThrows(RuntimeException.class, () -> SILab2.checkCart(items(new Item("Item1", "01234a", 500, 0.2f)), 1));
+        assertTrue(exception.getMessage().contains("Netocen barkod!"));
         assertFalse(SILab2.checkCart(items(new Item("Item1", "12345", 100, -1)), 100));
     }
     @Test
-    void MultipleConditions(){
-        assertFalse(SILab2.checkCart(items(new Item("","12345",250,15)),2));
-        assertFalse(SILab2.checkCart(items(new Item("","12345",350,0)),2));
-        assertFalse(SILab2.checkCart(items(new Item("","12345",400,15)),2));
-        assertFalse(SILab2.checkCart(items(new Item("","012345",400,15)),2));
+    void MultipleCondition(){
+        assertFalse(SILab2.checkCart(items(new Item("", "123", 350, 0.2f)),2));
+        assertFalse(SILab2.checkCart(items(new Item("", "123", 350, 0)),2));
+        assertTrue(SILab2.checkCart(items(new Item("", "012", 350, 0.2f)),300));
+        assertFalse(SILab2.checkCart(items(new Item("", "012", 100, 0.2f)),2));
     }
 
 }
